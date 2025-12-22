8a942290a7830897c236544703a85e11aab90dcf
Nargees Syed <nargees2009@gmail.com>
2025-12-22 19:47:52 +0530

    part18

--- src/pages/QuizPlayerPage.tsx
+++ src/pages/QuizPlayerPage.tsx
@@ -201,7 +201,7 @@ const QuizPlayerPage = () => {
                             quiz={quiz}
                             highlightPlayerId={playerId}
                             animate={true}
-                            strategicTip={strategicTip}
+                            strategicTip={strategicTip}            
                         />
                     </div>
                 );
